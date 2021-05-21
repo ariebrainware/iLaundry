ba34c2e0dfb9f9c4e4ffc0897de2ce65fab95852
Pritamisergio <giobkc22@gmail.com>
2021-05-21 23:13:33 +0700

    initial commit

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('paket-laundry', 'Paket_controller@index');
+Route::get('paket-laudry/add', 'Paket_controller@add');
+Route::post('paket-laudry/add', 'Paket_controller@store');
+Route::get('paket-laundry/{id}', 'Paket_controller@edit');
+Route::put('paket-laundry/{id}', 'Paket_controller@update');
+Route::delete('paket-laundry/{id}', 'Paket_controller@delete');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
